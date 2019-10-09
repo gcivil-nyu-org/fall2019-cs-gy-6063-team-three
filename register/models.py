532e02ee460914ae7592b34bace287e9531a01e9
Varsha Sivakumar <vs2165@nyu.edu>
2019-10-09 18:32:12 -0400

    1. Update gitignore file to ignore ideas files (IntelliJ generated files), venv 2. Create models for Register

--- register/models.py
+++ register/models.py
@@ -1,3 +1,23 @@
+# Create your models here.
+
 from django.db import models
 
-# Create your models here.
+
+def auto_str(cls):
+    def __str__(self):
+        return '%s(%s)' % (
+            type(self).__name__,
+            ', '.join('%s=%s' % item for item in vars(self).items())
+        )
+    cls.__str__ = __str__
+    return cls
+
+@auto_str
+class Register(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.CharField(max_length=200)
+    current_school = models.CharField(max_length=100)
+    borough = models.CharField(max_length=20)
+    password = models.CharField(max_length=8)
+
