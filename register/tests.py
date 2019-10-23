31ee6366a4cba6edec9e49664420345fc1f7f327
Varsha Sivakumar <vs2165@nyu.edu>
2019-10-23 00:19:55 -0400

    Add form test for Admission staff

--- register/tests.py
+++ register/tests.py
@@ -1,10 +1,11 @@
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 from OneApply.constants import UserType
+from register.forms import AdminStaffRegisterForm
 from register.models import Admin_Staff
 
 
-class AdmissionStaffTest(TestCase):
+class AdmissionStaffViewTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.adminStaff = {
@@ -18,11 +19,27 @@ class AdmissionStaffTest(TestCase):
             "confirm_password": "Jenny@1234",
         }
 
-    def test_register_admin(self):
+    def test_valid_register_admin(self):
         url = reverse("register:register_user", args=[UserType.ADMIN_STAFF])
         response = self.client.post(url, self.adminStaff)
-        # print(Admin_Staff.objects.all())
         self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(Admin_Staff.objects.get(username="jwang"))
+
+    def test_invalid_register_admin(self):
+        self.adminStaff = {
+            "username": "JWANG",
+            "first_name": "Jenny",
+            "last_name": "Wang",
+            "email_address": "jenny.wang@gmail.com",
+            "school": "NYU",
+            "supervisor_email": "jack.w@nyu.edu",
+            "input_password": "Jenny@1234",
+            "confirm_password": "Jenny@1234",
+        }
+        url = reverse("register:register_user", args=[UserType.ADMIN_STAFF])
+        response = self.client.post(url, self.adminStaff)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Admin_Staff.objects.filter(username="jwang").count(), 0)
 
     def tearDown(self):
         Admin_Staff.objects.all().delete()
@@ -60,3 +77,49 @@ class AdmissionStaffModelTest(TestCase):
         self.create_admission_staff()
         response = Admin_Staff.objects.filter(username="jwang").delete()
         self.assertIsNotNone(response)
+
+
+class AdmissionsFormTest(TestCase):
+    def test_admission_staff_valid_form(self):
+        data = {
+            "username": "jwang",
+            "first_name": "Jenny",
+            "last_name": "Wang",
+            "email_address": "jenny.wang@gmail.com",
+            "school": "NYU",
+            "supervisor_email": "jack.w@nyu.edu",
+            "input_password": "Jenny@1234",
+            "confirm_password": "Jenny@1234",
+        }
+        form = AdminStaffRegisterForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_password(self):
+        data = {
+            "username": "jwang",
+            "first_name": "Jenny",
+            "last_name": "Wang",
+            "email_address": "jenny.wang@gmail.com",
+            "school": "NYU",
+            "supervisor_email": "jack.w@nyu.edu",
+            "input_password": "Jenny1234",
+            "confirm_password": "Jenny1234",
+        }
+        form = AdminStaffRegisterForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertFalse("input_password" in form.cleaned_data)
+
+    def test_mismatch_password(self):
+        data = {
+            "username": "jwang",
+            "first_name": "Jenny",
+            "last_name": "Wang",
+            "email_address": "jenny.wang@gmail.com",
+            "school": "NYU",
+            "supervisor_email": "jack.w@nyu.edu",
+            "input_password": "Jenny@1234",
+            "confirm_password": "Jenny@1234567",
+        }
+        form = AdminStaffRegisterForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertFalse("confirm_password" in form.cleaned_data)
