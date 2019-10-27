b6c88f651b6f56a8e32fe240663af9958fadf63b
jinchuhan2009 <cj1436@nyu.edu>
2019-10-27 15:23:17 -0400

    tab feature

--- logIn/views.py
+++ logIn/views.py
@@ -5,6 +5,10 @@ from OneApply.constants import UserType
 
 
 def login_user(request, user_type):
+    context = {
+        "constant_ut_student": UserType.STUDENT,
+        "constant_ut_adminStaff": UserType.ADMIN_STAFF,
+    }
     login_error = False
     if request.method == "POST":
         form = LoginForm(request.POST)
@@ -14,13 +18,13 @@ def login_user(request, user_type):
             if user_type == UserType.STUDENT:
                 user = Student.objects.filter(username=username, password=password)
                 if user:
-                    return render(request, "dashboard/student.html")
+                    return render(request, "dashboard/student.html", context)
                 else:
                     login_error = True
             elif user_type == UserType.ADMIN_STAFF:
                 user = Admin_Staff.objects.filter(username=username, password=password)
                 if user:
-                    return render(request, "dashboard/admissionstaff.html")
+                    return render(request, "dashboard/admissionstaff.html", context)
                 else:
                     login_error = True
     else:
