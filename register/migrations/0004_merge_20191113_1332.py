11bb4b63d4b6b3629fa638a45e34852dec2c01c2
Patryk Pietraszko <pp2224@nyu.edu>
2019-11-13 13:35:04 -0500

    updated migrations format

--- register/migrations/0004_merge_20191113_1332.py
+++ register/migrations/0004_merge_20191113_1332.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('register', '0002_auto_20191113_0205'),
-        ('register', '0003_auto_20191112_2340'),
+        ("register", "0002_auto_20191113_0205"),
+        ("register", "0003_auto_20191112_2340"),
     ]
 
-    operations = [
-    ]
+    operations = []
