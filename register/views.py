c60849982ffe1c37ba61464453e39e9a2f9df6ed
Varsha Sivakumar <vs2165@nyu.edu>
2019-11-26 03:26:18 -0500

    Fix flake8 issue.

--- register/views.py
+++ register/views.py
@@ -1,7 +1,7 @@
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
