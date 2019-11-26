0cbd506828c7fcf0f192b48886c15f926994a3a0
Snehal Kenjale <snehalvk31@gmail.com>
2019-11-25 19:45:48 -0500

    fixed bugs, enhanced functionality for application tab

--- application/models.py
+++ application/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.core import validators
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from decimal import Decimal
 from phonenumber_field.modelfields import PhoneNumberField
 
-
 from register.models import Student
 from high_school.models import HighSchool, Program
+
+
+GENDER = [("", "Gender"), ("M", "Male"), ("F", "Female")]
+APPLICATION_STATUS = ["Rejected", "Accepted", "Submitted"]
 
 
 class HighSchoolApplication(models.Model):
@@ -14,32 +17,59 @@ class HighSchoolApplication(models.Model):
     user = models.ForeignKey(
         Student, on_delete=models.CASCADE, related_name="high_school_app_user"
     )
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, verbose_name="First Name")
+    last_name = models.CharField(max_length=50, verbose_name="Last Name")
     email_address = models.EmailField(
-        max_length=50, validators=[validators.validate_email]
+        max_length=50, validators=[validators.validate_email], verbose_name="Email ID"
     )
-    phoneNumber = PhoneNumberField()
-    address = models.CharField(max_length=100)
-    gender = models.CharField(max_length=15)
-    date_of_birth = models.DateField()
+    phoneNumber = PhoneNumberField(verbose_name="Phone number")
+    address = models.CharField(max_length=100, verbose_name="Address")
+    gender = models.CharField(max_length=15, verbose_name="Gender")
+    date_of_birth = models.DateField(verbose_name="Date of Birth")
     gpa = models.DecimalField(
-        max_digits=3, decimal_places=2, validators=[MinValueValidator(Decimal("0.00"))]
+        max_digits=3,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(Decimal("0.00")),
+            MaxValueValidator(Decimal("4.00")),
+        ],
+        verbose_name="GPA",
+        default=Decimal("0.00"),
+        blank=True,
     )
-    parent_name = models.CharField(max_length=100)
-    parent_phoneNumber = PhoneNumberField()
+    parent_name = models.CharField(max_length=100, verbose_name="Parent/Guardian Name")
+    parent_phoneNumber = PhoneNumberField(verbose_name="Parent/Guardian Phone number")
     school = models.ForeignKey(
-        HighSchool, on_delete=models.CASCADE, related_name="app_school"
+        HighSchool,
+        on_delete=models.CASCADE,
+        related_name="app_school",
+        verbose_name="School",
     )
-    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    program = models.ForeignKey(
+        Program, on_delete=models.CASCADE, verbose_name="Program"
+    )
     is_draft = models.BooleanField(default=True)
-    submitted_date = models.DateTimeField()
+    submitted_date = models.DateTimeField(verbose_name="Submitted")
     application_status = models.IntegerField(
-        default=2
+        default=2, verbose_name="Application Status"
     )  # 2 = "pending"; 1 = "accepted"; 0 ="rejected"
 
     def get_fields(self):
-        return [
-            (field.name, field.value_to_string(self))
-            for field in HighSchoolApplication._meta.fields
-        ]
+        x = []
+        for field in self._meta.fields:
+            if field.name not in ["id", "application_number", "user", "is_draft"]:
+                if field.name == "gender":
+                    try:
+                        value = [i[1] for i in GENDER if i[0] == self.gender][0]
+                    except Exception:
+                        value = field.value_from_object(self)
+                elif field.name == "school":
+                    value = str(self.school)
+                elif field.name == "program":
+                    value = str(self.program)
+                elif field.name == "application_status":
+                    value = APPLICATION_STATUS[self.application_status]
+                else:
+                    value = field.value_from_object(self)
+                x.append((field.verbose_name, value))
+        return x
