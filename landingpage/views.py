f4b1782c01fd8bb013990273c323175f33af5753
Varsha Sivakumar <vs2165@nyu.edu>
2019-11-25 23:09:00 -0500

    Display dashboard specific to the user type and display the landing page only after the user logs out

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from OneApply.constants import UserType
 
 
 # Create your views here.
+
 def index(request):
+    if request.session.get("is_login", None):
+        return redirect("dashboard:dashboard")
     context = {
         "constant_ut_student": UserType.STUDENT,
         "constant_ut_adminStaff": UserType.ADMIN_STAFF,
