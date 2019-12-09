4d6f01219a40b93a7124133942e8134b1cce8205
Abe <abhish8888@gmail.com>
2019-12-09 01:23:56 -0500

    format issues

--- register/migrations/0005_upgrade_password.py
+++ register/migrations/0005_upgrade_password.py
@@ -4,25 +4,21 @@ from django.contrib.auth.hashers import make_password
 
 # for updating the current passwords to pbkdf2 algorithm
 def forwards_func(apps, schema_editor):
-    Students = apps.get_model('register', 'Student')
+    Students = apps.get_model("register", "Student")
     student_users = Students.objects.exclude(password__startswith="pbkdf2_")
     for user in student_users:
         user.password = make_password(user.password)
-        user.save(update_fields=['password'])
+        user.save(update_fields=["password"])
 
-    Admin_Staff = apps.get_model('register', 'Admin_Staff')
+    Admin_Staff = apps.get_model("register", "Admin_Staff")
     admin_staff_users = Admin_Staff.objects.exclude(password__startswith="pbkdf2_")
     for user in admin_staff_users:
         user.password = make_password(user.password)
-        user.save(update_fields=['password'])
+        user.save(update_fields=["password"])
 
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('register', '0004_merge_20191113_1332'),
-    ]
+    dependencies = [("register", "0004_merge_20191113_1332")]
 
-    operations = [
-        migrations.RunPython(forwards_func),
-    ]
+    operations = [migrations.RunPython(forwards_func)]
